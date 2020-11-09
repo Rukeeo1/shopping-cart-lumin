8a51dcb7b8ab854a780dee7b431adf487922c15e
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-11-09 22:14:20 +0100

    chore: initial setup and navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Navbar from 'components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Navbar />
+  
     </div>
   );
 }
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './index.scss';
+
+export default function Navbar() {
+  return (
+    <nav className="nav-bar">
+      <div className="nav-bar__brand">
+        <h1>lumin</h1>
+        <ul>
+          <li>Shop</li>
+          <li>Learn</li>
+        </ul>
+      </div>
+      <div className="nav-bar__cart">
+        <ul>
+          <li>Account</li>
+          <li>
+            <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+            <span className="item-num">4</span>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
