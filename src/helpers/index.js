de5381ff4c94e6799dd80ec525f75924eddd4886
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-11-12 13:50:59 +0100

    chore: add helper functions

---
+++ src/helpers/index.js
@@ -0,0 +1,14 @@
+import getSymbolFromCurrency from 'currency-symbol-map';
+
+/**
+ * couldn't find typings for getSymbolFromCurrency,
+ * not sure it has typescript support yet. so for convenience i opted to use a normal js file
+ */
+export { getSymbolFromCurrency };
+
+export const sumCartItems = (cartItems) => {
+  return cartItems.reduce(function (accumlator, cartItem) {
+    const totalPriceOfIndividualItem = cartItem.price * cartItem.quantity;
+    return accumlator + totalPriceOfIndividualItem;
+  }, 0);
+};
