26e908b094eaca7bf1fe8a1f69a43b174b8885bf
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-11-12 22:49:23 +0100

    chore: add currency

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -1,18 +1,22 @@
 import React, { useContext } from 'react';
 import { IProductDetails } from 'components/Product';
-import './index.scss';
 import AppContext from 'context';
+import './index.scss';
 
 interface ICartItem {
   product: IProductDetails;
+  defaultCurrency: string;
 }
 
-export default function CartItem({ product }: ICartItem) {
+export default function CartItem({ product, defaultCurrency }: ICartItem) {
   const { title, price, image_url, quantity, id } = product;
   const context = useContext(AppContext);
 
   return (
     <div className="cart-item mb-3 mt-3">
+      <span onClick={() => context?.removeItemFromCart(id)}>
+        <i className="fa fa-times" aria-hidden="true"></i>
+      </span>
       <div className="d-flex flex-column justify-content-between cart-item__title-wrapper">
         <h1>{title}</h1>
         <div className="d-flex justify-content-between  align-items-center">
@@ -22,11 +26,7 @@ export default function CartItem({ product }: ICartItem) {
                 context?.increaseOrDecreaseCartItem(id, 'decrement')
               }
             >
-              <i
-                className="fa fa-minus mr-2"
-                aria-hidden="true"
-                onClick={() => {}}
-              ></i>
+              <i className="fa fa-minus mr-2" aria-hidden="true"></i>
             </span>
             <span>{quantity}</span>
             <span
@@ -37,7 +37,9 @@ export default function CartItem({ product }: ICartItem) {
               <i className="fa fa-plus ml-2" aria-hidden="true"></i>
             </span>
           </div>
-          <p className="mb-0 cart-item__price">${price}</p>
+          <p className="mb-0 cart-item__price">
+            {defaultCurrency} {price}
+          </p>
         </div>
       </div>
 
