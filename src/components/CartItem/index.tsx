d2ac6c09fd71a0290b206120ad3a6fe92b3593df
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-11-12 13:49:49 +0100

    ft: create cart item

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { IProductDetails } from 'components/Product';
 import './index.scss';
+import AppContext from 'context';
 
-const url =
-  'https://d1b929y2mmls08.cloudfront.net/luminskin/img/new-landing-page/moisturizing-balm.png'
+interface ICartItem {
+  product: IProductDetails;
+}
 
-export default function CartItem() {
+export default function CartItem({ product }: ICartItem) {
+  const { title, price, image_url, quantity, id } = product;
+  const context = useContext(AppContext);
+
   return (
-    <div className="cart-item">
+    <div className="cart-item mb-3 mt-3">
       <div className="d-flex flex-column justify-content-between cart-item__title-wrapper">
-        <h1>Dark Circle Defense</h1>
+        <h1>{title}</h1>
         <div className="d-flex justify-content-between  align-items-center">
           <div className="cart-item__controls">
-            <i className="fa fa-minus mr-2" aria-hidden="true" onClick={() => {}}></i>
-            <span>1</span>
-            <i className="fa fa-plus ml-2" aria-hidden="true" onClick={() => {}}></i>
+            <span
+              onClick={() =>
+                context?.increaseOrDecreaseCartItem(id, 'decrement')
+              }
+            >
+              <i
+                className="fa fa-minus mr-2"
+                aria-hidden="true"
+                onClick={() => {}}
+              ></i>
+            </span>
+            <span>{quantity}</span>
+            <span
+              onClick={() =>
+                context?.increaseOrDecreaseCartItem(id, 'increment')
+              }
+            >
+              <i className="fa fa-plus ml-2" aria-hidden="true"></i>
+            </span>
           </div>
-          <p className="mb-0 cart-item__price">$29.00</p>
+          <p className="mb-0 cart-item__price">${price}</p>
         </div>
       </div>
 
       <div className="cart-item__image-wrapper">
-        <img src={url}  alt="ruke"/>
+        <img src={image_url} alt={title} />
       </div>
     </div>
   );
