d3eb68e839391cf2c515b9ea2d23ff1a3472a270
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-11-12 13:48:30 +0100

    chore: bring in appollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import './App.scss';
 import Navbar from 'components/Navbar';
-import LandingPage from 'views/Home';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from 'views/Home';
+import { AppContextProvider } from 'context';
+
+const client = new ApolloClient({
+  uri: 'https://pangaea-interviews.now.sh/api/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <main>
-        <Home />
-      </main>
-    </div>
+    <ApolloProvider client={client}>
+      <AppContextProvider>
+        <Navbar />
+        <main>
+          <Home />
+        </main>
+      </AppContextProvider>
+    </ApolloProvider>
   );
 }
 
